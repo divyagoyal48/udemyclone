e2c01e44fd5e6346a44ca834013f9920c29f3d60
divyagoyal48 <83334457+divyagoyal48@users.noreply.github.com>
2022-01-10 20:06:27 +0530

    commit

--- Models/student.js
+++ Models/student.js
@@ -5,8 +5,8 @@ const studentSchema=new mongoose.Schema({
     StudentName:{type:String },
     Email:{type:String },
     Mobile:{type:String },
-    PWD:{type:String }
-    
+    PWD:{type:String },
+    EnrollCourses:{type:[String] }
 });
 
 module.exports=mongoose.model("Student",studentSchema,"Students");
--- Models/trainer.js
+++ Models/trainer.js
@@ -1,10 +1,12 @@
 const mongoose=require("mongoose");
+const { GetAllCourses } = require("../controllers/courses");
 
 const userSchema=new mongoose.Schema({
     TrainerName:{type:String,  required:true, unique:true },
     Email:{type:String,  required:true, unique:true },
     Mobile:{type:String,  required:true, unique:true },
-    PWD:{type:String,  required:true, unique:true }
+    PWD:{type:String,  required:true, unique:true },
+    COurses:{type:[String]}
     
 })
 const Model=new mongoose.model("Trainers",userSchema)
